5f3131734045e0fe762ba7c6a7061a30202e224b
seniorflacko <cristianalemanfuentes@gmail.com>
2017-08-07 12:58:26 -0500

    Arrays,Tuples,Enums,Any

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ myTrueName = "Flacko!";
 //array of string
 var hobbies = ["Cooking", "Sports"];
 console.log(typeof hobbies[0]);
+//tuples
+var address = ["JJ Cantu", 218];
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+//Any can take various forms can be 
+// String and then Object and then a Number
+var car = "BMW";
+console.log(car);
+car = { car: "BMW", series: 3 };
+console.log(car);
--- script.ts
+++ script.ts
@@ -30,3 +30,27 @@ myTrueName = "Flacko!"
 let hobbies:string[] = ["Cooking","Sports"]
 console.log(typeof hobbies[0])
 
+//tuples
+let address: [string,number] = ["JJ Cantu",218]
+
+//enum
+enum Color {
+    Gray,
+    Green = 100,
+    Blue
+}
+
+let myColor:Color = Color.Green
+console.log(myColor)
+
+
+// any
+
+//Any can take various forms can be 
+// String and then Object and then a Number
+
+let car:any = "BMW"
+console.log(car)
+car = {car:"BMW",series:3}
+console.log(car)
+
