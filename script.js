8fff5282ca8364c0429e01eb62ce2df9b42923d2
seniorflacko <cristianalemanfuentes@gmail.com>
2017-08-07 12:35:14 -0500

    Tipos explicitos e implicitos

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("It Works");
+/* Tipos implicitos  En su declaracion */
+// string
+var myName = 'Cris';
+// number
+var myAge = 21.6;
+//boolean
+var hasBoobies = true;
+/* Tipos explicitos */
+//type any (No recomendado)
+var myRealAge;
+myRealAge = 21;
+myRealAge = "21";
+//
+var myTrueAge;
+myTrueAge = 21;
--- script.ts
+++ script.ts
@@ -1,1 +1,23 @@
-console.log("It Works")
+
+/* Tipos implicitos  En su declaracion */
+
+// string
+let myName = 'Cris'
+
+// number
+let myAge = 21.6
+
+//boolean
+let hasBoobies = true
+
+
+/* Tipos explicitos */
+
+//type any (No recomendado)
+let myRealAge
+myRealAge = 21
+myRealAge = "21"
+
+//
+let myTrueAge:number
+myTrueAge = 21
