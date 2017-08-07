c2132227f53eaf3ab3e99800249d0aa85953cf8a
seniorflacko <cristianalemanfuentes@gmail.com>
2017-08-07 15:43:11 -0500

    Funcion Arrow con 0,1,mayor a 1 parametro con return y sin return

--- app.js
+++ app.js
@@ -12,3 +12,30 @@ function reset() {
 }
 reset();
 console.log(variable);
+// Arrow Functions
+console.log("ARROW FUNCTION");
+/* Las viejas funciones */
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(1, 1));
+/*
+    Las Funciones arrow se igualan directamente a los parmetros
+    Con el operador flecha se especifica el body de la funcion
+
+    Si solo es un return puede ir en una misma linea y sin la palabra return
+
+ */
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(3, 3));
+/* Funcion arrow sin parametros */
+var greet = function () {
+    console.log("hello!!!!");
+};
+greet();
+/* Funcion arrow con un parametro */
+var greetFriend = function (friend) { return console.log("hello!!!!" + friend); };
+greetFriend("Pancho");
+/* Funcion arrow mas de un parametro */
+var greetFriends = function (friend1, friend2) { return console.log("hello!!!!" + friend1 + " " + friend2); };
+greetFriends("Pancho", "Cris");
--- app.ts
+++ app.ts
@@ -16,4 +16,42 @@ function reset(){
 reset()
 console.log(variable)
 
+// Arrow Functions
 
+console.log("ARROW FUNCTION")
+
+
+/* Las viejas funciones */
+const addNumbers = function(number1:number,number2:number):number{
+    return number1 + number2
+}
+
+console.log(addNumbers(1,1))
+
+/* 
+    Las Funciones arrow se igualan directamente a los parmetros 
+    Con el operador flecha se especifica el body de la funcion 
+
+    Si solo es un return puede ir en una misma linea y sin la palabra return
+
+ */
+const multiplyNumbers = (number1:number,number2:number)=> number1*number2
+
+console.log(multiplyNumbers(3,3))
+
+/* Funcion arrow sin parametros */
+const greet = () =>  {
+    console.log("hello!!!!")
+}
+
+greet()
+
+/* Funcion arrow con un parametro */
+const greetFriend = friend => console.log("hello!!!!"+ friend)
+
+greetFriend("Pancho")
+
+/* Funcion arrow mas de un parametro */
+const greetFriends = (friend1:string,friend2:string) => console.log("hello!!!!"+ friend1 +" "+ friend2)
+
+greetFriends("Pancho","Cris")
